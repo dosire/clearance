8e0174cecbd20b0ef579ec735ac41bba60e3fcbf
Karen Sijbrandij <kxesijbrandij@yahoo.co.uk>
2012-06-06 13:22:02 +0200

    Added locale to clearance routes. #415

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Rails.application.routes.draw do
-  resources :passwords,
-    :controller => 'clearance/passwords',
-    :only       => [:new, :create]
-
-  resource  :session,
-    :controller => 'clearance/sessions',
-    :only       => [:new, :create, :destroy]
-
-  resources :users, :controller => 'clearance/users', :only => [:new, :create] do
-    resource :password,
+  scope ":locale", :locale => /en|nl/ do
+    resources :passwords,
       :controller => 'clearance/passwords',
-      :only       => [:create, :edit, :update]
+      :only       => [:new, :create]
+
+    resource  :session,
+      :controller => 'clearance/sessions',
+      :only       => [:new, :create, :destroy]
+
+    resources :users, :controller => 'clearance/users', :only => [:new, :create] do
+      resource :password,
+        :controller => 'clearance/passwords',
+        :only       => [:create, :edit, :update]
+    end
   end
 
-  match 'sign_up'  => 'clearance/users#new', :as => 'sign_up'
-  match 'sign_in'  => 'clearance/sessions#new', :as => 'sign_in'
-  match 'sign_out' => 'clearance/sessions#destroy', :via => :delete, :as => 'sign_out'
+  # match ':locale/sign_up'  => 'clearance/users#new', :as => 'sign_up', :locale => /en|nl/
+  match ':locale/sign_in'  => 'clearance/sessions#new', :as => 'sign_in', :locale => /en|nl/
+  match ':locale/sign_out' => 'clearance/sessions#destroy', :via => :delete, :as => 'sign_out', :locale => /en|nl/
 end
